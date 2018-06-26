c291936fbec0b4d46f044593d5ef49e32a1eeb08
Li Jin <jinli826@gmail.com>
2018-06-25 22:38:22 -0500

    add sass

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var hbs = require('hbs');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var lessMiddleware = require('less-middleware');
+var sassMiddleware = require('node-sass-middleware');
 var logger = require('morgan');
 
 var app = express();
@@ -19,7 +20,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(lessMiddleware(path.join(__dirname, 'public/less')));
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public', 'sass', 'stylesheets'),
+    dest: path.join(__dirname, 'public', 'sass', 'stylesheets'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/sass/stylesheets'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes file
